822957792ebf26ae95336289fb654f9cdb7c7a4b
hrutikaborade <86976843+hrutikaborade@users.noreply.github.com>
2022-01-06 18:09:46 +0530

    Create file.sql

---
+++ file.sql
@@ -0,0 +1,4 @@
+CREATE TABLE studentData(Name varchar(20), Roll_No integer, Address varchar(40), Mobile_Number bigint primary key NOT NULL, PAN_Number varchar(10));
+
+INSERT INTO studentData values('Hrutika', 20111003, 'Nasik', 9423085429, 'ELPN238499');
+INSERT INTO studentData values('Shraddha', 20111008, 'Pune', 9348328321, 'KLPS233526');
